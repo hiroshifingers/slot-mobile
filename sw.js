ee906ec8888b6c545b0163f1f7f6f64caf39f3f9
hiroshi <hiroshi@showjin.co.jp>
2026-07-15 12:33:06 +0900

    update: 2026-07-15 12:33 予測スナップショット更新

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 予測シェルの Service Worker: シェル+予測をオフライン化 / data.json はネット優先 */
-const CACHE = 'slot-mobile-20260714-1843-db4bac5619';
+const CACHE = 'slot-mobile-20260715-1232-bd80510adb';
 const ASSETS = [
   './',
   './index.html',
